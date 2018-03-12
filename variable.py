5e34b90733e528be110dac9dda5b980cacd76538
ClaryCo <171wita01@o365.htl-leoben.at>
2018-03-12 15:01:50 +0100

    variable

--- variable.py
+++ variable.py
@@ -1,4 +1,5 @@
 # variable
+
 a = 10
 b = 20
 c = 30
@@ -6,3 +7,39 @@ c = 30
 summe = a + b
 
 print("Summe von ",a," + ", b, " = ",summe)
+
+aktion = input ("Welche Aktion willst du?: (+/-/*/:)" )
+print("Aktion",aktion, " wird ausgeführt...")
+
+if (aktion == "*"):
+    print("Multiplikation")
+
+zahl = input("Welche Zahl wählst du?")
+zahl = int (zahl)
+
+zahl2 = int("23")
+
+zahlA = "123"
+zahlB = "456"
+print(zahlA+zahlB)
+# Ausgabe: 123456,weil es sich hier um einen String handelt!
+
+zahlC = 987
+text = str(zahlC)
+
+
+#Taschenrechner
+aktion = input ("Welche Aktion willst du?: (+/-/*/:)" )
+zahl1 = input ("Gib deine 1. Zahl ein: ")
+zahl2 = input ("Gib deine 2. Zahl ein: ")
+
+if ( aktion == "+" ):
+    ausgabe = int(zahl1)+int(zahl2)
+if( aktion == "-"):
+    ausgabe = int (zahl1)-int(zahl2)
+if(aktion == " * "):
+    ausgabe = int (zahl1)*int(zahl2)
+if (aktion == " : "):
+    ausgabe = int(zahl1):int(zahl2)
+    
+print(ausgabe)
